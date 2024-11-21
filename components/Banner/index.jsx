ea5d6beb480c8a6db72162c20c6b6a0aba3b6b5f
yessbertel <ybertel0307@gmail.com>
2024-11-21 14:33:51 -0500

    Ultimos cambios

--- components/Banner/index.jsx
+++ components/Banner/index.jsx
@@ -1,49 +1,37 @@
-import homeMobile1 from "./images/mobile.webp";
-import homeDesktop1 from "./images/desktop.webp";
-import bgImg from "./images/bg.png";
-import top from "./banner.module.scss";
+import banner from "./banner.module.scss";
 import Simulation from "@/components/Simulation";
+import bgImg from "./images/bg.png";
 
-const Banner = () => {
+const TopBanner = () => {
   return (
-    <section className={top.main}>
-      <picture>
-        <source srcSet={homeDesktop1.src} media="(min-width: 480px)"  
-        height={830}/>
-        <img
-          src={homeMobile1.src}
-          alt="Videre Rooftop Restaurant & Bar"
-          loading="eager"
-          className="img-fluid"
-          width={379}
-          height={750}
-        />
-      </picture>
-
-      <div className={`${top.captionHome} container`}>
-        <h1 className={top.title}>
-          <span className={top.title1}>Crédito</span>
-          <span className={top.title2}>100%</span>
-          <span className={top.title3}>en línea</span>
-        </h1>
-        <img 
-          srcSet={bgImg.src}
-          alt="Credi Plus"
-          width={550}
-          height={400}
-          loading="eager"
-        />
-        <a href="https://trabajadores.crediplus.com.co" passHref>
-          <button className={top.button}>Instalar App</button>
-        </a>
-        {/* <div className={top.contentText}> */}
-          <p>En <strong>Crediplus</strong> cuentas con el crédito en línea <strong>más confiable, ágil y adaptable del mercado.</strong></p>
-        {/* </div> */}
+    <section className={banner.main}>
+      <div className="container">
+        <div className={banner.captionHome}>
+          <h1 className={banner.title}>
+            <span className={banner.title1}>Crédito</span><br />
+            <span className={banner.title2}>100%</span><br />
+            <span className={banner.title3}>en línea</span>
+          </h1>
+          <img
+            srcSet={bgImg.src}
+            alt="Credi Plus"
+            width={620}
+          />
+          <a href="https://trabajadores.crediplus.com.co/" passHref target="_blank">
+            <button className={banner.button}>Instalar App</button>
+          </a>
+          {/* <div className={banner.contentText}> */}
+          <p>
+            En <strong>Crediplus</strong> cuentas con el crédito en línea{" "}
+            <strong>más confiable, ágil y adaptable del mercado.</strong>
+          </p>
+          <div className={banner.simulation}>
+          <Simulation/>
+          </div>
+        </div>
       </div>
-      <Simulation />
     </section>
   );
 };
 
-export default Banner;
-
+export default TopBanner;
